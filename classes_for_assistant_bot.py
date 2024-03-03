d37d915bfda9f6edfae964273c134d2f6695896b
Marina Rozhdestvenska <rozhd.buro@gmail.com>
2024-03-03 22:36:06 +0100

    modified:   classes_for_assistant_bot.py

--- classes_for_assistant_bot.py
+++ classes_for_assistant_bot.py
@@ -16,26 +16,43 @@ class Phone(Field):
         super().__init__(value)
         if not self.is_valid_phone():
             raise ValueError("Invalid phone number")
+    def is_valid_phone(self):
+
+        return len(self.value) == 10 and self.value.isdigit()
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
 
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p.value
+
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
 
 class AddressBook(UserDict):
-    def add_record(self, name):
+    def add_record(self, record):
         self.data[record.name.value] = record
-    def delete_record(self, name):
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
         del self.data[name]
-    def lookup_record(self, name):
-        return self.data[name]
-    
-    def __str__(self):
-        
-    
-    
